2ea38c82cc86d1ac1f90b67b57512f0dfe3acda6
PJ <albanopatj@gmail.com>
2023-02-05 13:24:37 -0500

    Add styling adjust <br> in html

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ console.log(msg);
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
 console.log(alphabet);
+
+function message (form) {
+    let inputVal = form.inputbox.value;
+}
