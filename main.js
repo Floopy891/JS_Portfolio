023afab46f537ce4b232b642de8bb639d7ac7dbe
PJ <albanopatj@gmail.com>
2023-02-05 16:04:13 -0500

    add encoder function first vers

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-let msg = 'Secret Agent Man';
-console.log(msg);
-
+const form = document.forms[0];
+const input = document.getElementById('inmsg');
+const output = document.getElementById('cipher');
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
-console.log(alphabet);
 
-const form = document.forms[0];
-const output = document.getElementById('output');
+const caesar = (alphabet) => {
+    let rand = Math.floor(Math.random()*1);
+    let caeCipher = alphabet.map((x) => x + rand);
+    return caeCipher; 
+}
+
+const encoder = (input, caesar) => {
+    for(let i = 0; i < input.length; i++) {
+        let int = parseInt(input[i], 26);
+        return int;
+    }
+    for(let j = 0; j < caesar.length; j++) {
+        let int2 = parseInt(caesar[j], 26);
+        return int2;
+    }
+    if (this.int === this.int2) {
+        return this.int[i + 1];
+    } else {
+        return this.int2;
+    }
+}
