35abaea724768cc9fb5e6cbdf608e7d9b87b653a
PJ <albanopatj@gmail.com>
2023-02-05 12:25:39 -0500

    Make alphabet arrays

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 let msg = 'Secret Agent Man';
 console.log(msg);
+
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabet = alpha.map((x) => String.fromCharCode(x));
+console.log(alphabet);
