24a5130a380cc67d38ede0bef11a5232e0b92591
PJ <albanopatj@gmail.com>
2023-02-05 18:29:52 -0500

    add updates after web review

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
 
-const newAlpha = "";
+let newAlpha = "";
 
 const shift = (n) => {
-    for (let i = 0; i < alphabet.length(); i++) {
-        let offset = (i + n) % alphabet.length;
-        newAlpha += alphabet[offset];
-    }
+for (let i = 0; i < alphabet.length; i++) {
+let offset = (i + n) % alphabet.length;
+newAlpha += alphabet[offset];
+}
 }
 
-import
-    function encoder(input) {
-        let result = "";
-        input = input.toLowerCase();
-        for (let i = 0; i < input.length; i++) {
-            let index = alphabet.indexOf(input[i]);
-            result += newAlpha[index];
-        }
-        return result;
-    }
+const encoder = (input) => {
+let result = "";
+input = input.toLowerCase();
+shift(3);
+for (let i = 0; i < input.length; i++) {
+let index = alphabet.indexOf(input[i]);
+result += newAlpha[index];
+}
+return result;
+}
 
-console.log(encoder());
+console.log(encoder("HELLO"));
