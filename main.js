05a44ddd2624bd8ed2ebb24c86152a6380afa45c
PJ <albanopatj@gmail.com>
2023-02-05 13:34:56 -0500

    add script tag to link files and change form variables

--- main.js
+++ main.js
@@ -5,6 +5,5 @@ const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
 console.log(alphabet);
 
-function message (form) {
-    let inputVal = form.inputbox.value;
-}
+const form = document.forms[0];
+const output = document.getElementById('output');
