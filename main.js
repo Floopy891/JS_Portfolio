0d19a2ba8820c2e6b5e7ef82e996ced437095960
PJ <albanopatj@gmail.com>
2023-02-05 18:20:47 -0500

    Tidy code and work on DOM functions

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-const form = document.forms[0];
-const input = document.getElementById('inmsg');
-const cipher = document.createElement('cipher');
-
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
 
 const newAlpha = "";
 
 const shift = (n) => {
-    for(let i = 0; i < alphabet.length(); i++) {
+    for (let i = 0; i < alphabet.length(); i++) {
         let offset = (i + n) % alphabet.length;
-		newAlpha += alphabet[offset];
+        newAlpha += alphabet[offset];
     }
 }
 
-const encoder = (input) => {
-    let result = "";
-    input = input.toLowerCase();
-    for (let i = 0; i < input.length; i++){
-        let index = alphabet.indexOf(input[i]);
-        result += newAlpha[index];
+import
+    function encoder(input) {
+        let result = "";
+        input = input.toLowerCase();
+        for (let i = 0; i < input.length; i++) {
+            let index = alphabet.indexOf(input[i]);
+            result += newAlpha[index];
+        }
+        return result;
     }
-    return result;
-}
+
+console.log(encoder());
